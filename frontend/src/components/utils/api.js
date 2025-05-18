41b7ffa20f4f098e43b7f359b192214c4bbd9c8b
saad khan <saadkhan108b@gmail.com>
2025-05-18 15:34:47 +0530

    configured env var in frontend

--- frontend/src/components/utils/api.js
+++ frontend/src/components/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {getToken} from './auth.js'
 const api = axios.create({
-    baseURL : import.meta.env.BACKEND_HTTP_URL
+    baseURL : import.meta.env.VITE_BACKEND_HTTP_URL
 });
 
 api.interceptors.request.use((config) =>{
