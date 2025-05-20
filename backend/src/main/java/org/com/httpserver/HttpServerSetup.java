6d5ea058d4fd14a3cd1f3e99fc3d8b001188f969
saad khan <saadkhan108b@gmail.com>
2025-05-20 22:09:50 +0530

    Added a handler to process ping request at backend

--- backend/src/main/java/org/com/httpserver/HttpServerSetup.java
+++ backend/src/main/java/org/com/httpserver/HttpServerSetup.java
@@ -1,10 +1,12 @@
 package org.com.httpserver;
 
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.com.config.ConfigurationManager;
 import org.com.httpserver.handlers.AuthHandler;
+import org.com.util.HandlerUtil;
 import org.com.util.ThreadPoolManager;
 
 import java.io.IOException;
@@ -27,8 +29,31 @@ public class HttpServerSetup {
 
 
         // TODO -- Make handler for each path
+        /**
+         * This is lambda expression for handling ping request from frontend homepage to start the backend.
+         * It Just responds with 204 code no content, To indicate that backend is up and running
+         */
+        httpServer.createContext("/ping", exchange -> {
+
+            /*
+             * Due to JWT Auth token this simple ping request becomes preflighted request,
+             * and preflight request must be handled
+             */
+            HandlerUtil.corsHandler(exchange);
+            String method = exchange.getRequestMethod();
+            if (method.equalsIgnoreCase("OPTIONS")){
+                exchange.sendResponseHeaders(204, -1);
+                return;
+            }
+            try{
+                exchange.sendResponseHeaders(204,-1);
+                logger.info("Server recevied ping request");
+            } catch (IOException e) {
+                logger.fatal("Server cant send response +" + e.getMessage());
+            }
+        });
         httpServer.createContext("/auth", new AuthHandler());
-//        httpServer.createContext("/projects", new ProjectHanler());
+//        httpServer.createContext("/projects", new ProjectHandler());
 //        httpServer.createContext("/files", new FilesHandler());
 //        httpServer.createContext("/execute", new ExecuteHandler());
 //        httpServer.createContext("/collaborators", new CollaboratorsHandler());
